3727fbf2f9d1c8435e78ec315eb1018f45c344c5
vladlen714 <vladislavbolshakov23@gmail.com>
2022-04-19 08:16:23 +0700

    chaged server

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import storage
 import datetime
 import logging
 import os
+import cherrypy
 import pandas
 import requests
 from flask import Flask, request
@@ -14,6 +15,26 @@ from flask_sslify import SSLify
 TOKEN = "5219218963:AAFRuxk0G7RrYGAq6su7M5beww1RHl6KokY"
 APP_NAME = "catchstickbot"
 
+API_TOKEN = '5219218963:AAFRuxk0G7RrYGAq6su7M5beww1RHl6KokY'
+
+WEBHOOK_HOST = '217.25.89.150'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+
 app = Flask(__name__)
 sslify = SSLify(app)
 
@@ -21,20 +42,20 @@ logging.basicConfig(filename="logs.txt", level=logging.INFO)
 
 bot = telebot.TeleBot(TOKEN)
 
-bot.remove_webhook()
-bot.set_webhook(url=f"882659-cg05265.tmweb.ru/{TOKEN}")
-
-def send_message(chat_id, text):
-    method = "sendMessage"
-    url = f"https://api.telegram.org/bot{TOKEN}/{method}"
-    data = {"chat_id": chat_id, "text": text}
-    requests.post(url, data=data)
-
-@app.route("/"+TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates(
-        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+           'content-type' in cherrypy.request.headers and \
+           cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+            
 
 already_clicked = False
 
@@ -330,13 +351,24 @@ def get_storage(callback_query):
     logging.info("Got storage by {} ({})".format(
         str(callback_query.from_user.id), callback_query.from_user.username)+"\n----------------")
 
-@app.route("/", methods=['GET'])
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=f"882659-cg05265.tmweb.ru/{TOKEN}")
-    return "!", 200
+bot.remove_webhook()
 
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
-if __name__ == "__main__":
-    app.run(host=os.getenv('IP', '0.0.0.0'),
-            port=int(os.getenv('PORT', 4444)))
+# Disable CherryPy requests log
+access_log = cherrypy.log.access_log
+for handler in tuple(access_log.handlers):
+    access_log.removeHandler(handler)
+
+# Start cherrypy server
+cherrypy.config.update({
+    'server.socket_host'    : WEBHOOK_LISTEN,
+    'server.socket_port'    : WEBHOOK_PORT,
+    'server.ssl_module'     : 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
+})
+
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
