bf5e7424b2bf631ee7c5fd7bb74360fee2757e9a
vladlen714 <vladislavbolshakov23@gmail.com>
2022-04-19 08:32:39 +0700

    update clear_storage

--- dev_tools/clear_storage.py
+++ dev_tools/clear_storage.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 fieldnames = ['tgid', 'num', 'fio', 'time', 'longitude', 'latitude', 'status']
 data = pd.DataFrame(columns=fieldnames)
-data.to_xlsx("storage.xlsx")
+data.to_xlsx("../storage.xlsx")
